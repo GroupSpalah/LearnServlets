2d4832a6d2e0996fc262e848ad9d2fa3088fd6c8
Avalev <valevskii.a@gmail.com>
2024-05-07 18:31:30 +0200

    Init commit

---
+++ src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+
+}

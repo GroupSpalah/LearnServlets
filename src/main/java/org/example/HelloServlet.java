d950fde8218ad69134a7aa987ce12700a4eedea7
Avalev <valevskii.a@gmail.com>
2024-05-15 14:44:03 +0200

    Configure Tomcat

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -3,12 +3,23 @@ package org.example;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class HelloServlet extends HttpServlet {
+public class HelloServlet extends HttpServlet {// /pages/Hello.html
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
 
+        writer.println("Hello, Servlet, Get request!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+
+        writer.println("Hello, Servlet, Post request!");
     }
 
 }

3f60e4629d07af10c24275a90ca8791c915a4fa0
Mykhailo515 <mikhail.chursinov@gmail.com>
2024-08-10 12:06:16 +0200

    commit init

--- src/main/java/org/homeworks/misha/hw_20_06_2024/filter/TruckFilter.java
+++ src/main/java/org/homeworks/misha/hw_20_06_2024/filter/TruckFilter.java
@@ -1,36 +1,38 @@
 package org.homeworks.misha.hw_20_06_2024.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.homeworks.misha.hw_20_06_2024.domain.DriverM;
+import org.homeworks.misha.hw_20_06_2024.domain.TruckM;
 
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = "/driver/*")
-public class DriverFilter extends HttpFilter {
-
+public class TruckFilter extends HttpFilter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
+
         BufferedReader reader = servletRequest.getReader();
         ObjectMapper mapper = new ObjectMapper();
 
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        DriverM driverM = mapper.readValue(reader, DriverM.class);
+        TruckM truckM = mapper.readValue(reader, TruckM.class);
 
-        if (driverM.getAge() < 18) {
-            response.getWriter().print("Incorrect age");
+        if (truckM.getDate() < 2010) {
+            response.getWriter().print("Incorrect date");
             response.setStatus(403);
         } else {
             filterChain.doFilter(request, response);
         }
+
 
     }
 }
